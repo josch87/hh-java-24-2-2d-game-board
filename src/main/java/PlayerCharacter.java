5e613dadda1eed3b85c4832a01a9904407961601
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-04-18 11:23:23 +0200

    Add PlayerCharacter class and test with Junit dependency
    A new class PlayerCharacter has been added and its getX() method has been tested to return 0 using Junit. Junit dependency has been included in the pom.xml file to enable the testing.

---
+++ src/main/java/PlayerCharacter.java
@@ -0,0 +1,6 @@
+public class PlayerCharacter {
+
+    public static int getX() {
+        return 0;
+    }
+}
---
+++ src/test/java/PlayerCharacterTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerCharacterTest {
+    @Test
+    void getXReturn0() {
+        //GIVEN
+
+        //WHEN
+        int actual = PlayerCharacter.getX();
+
+        //THEN
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
