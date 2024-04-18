f71dfc325242071defc5267a7351121544bc8fed
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-04-18 11:27:16 +0200

    Add getY method and respective test
    A getY method has been added to the PlayerCharacter class. Corresponding test `getYReturn0` was included in the PlayerCharacterTest class to confirm the getY method returns 0 as expected.

--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -14,4 +14,16 @@ public class PlayerCharacterTest {
         int expected = 0;
         assertEquals(expected, actual);
     }
+
+    @Test
+    void getYReturn0() {
+        //GIVEN
+
+        //WHEN
+        int actual = PlayerCharacter.getY();
+
+        //THEN
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
